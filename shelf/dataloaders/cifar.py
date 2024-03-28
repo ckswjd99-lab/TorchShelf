5d4da975c36c86bc3946e40a315dd0961529aa3d
Chanjeong Park <ckswjd99@gmail.com>
2024-03-28 20:24:15 +0900

    dataset augmentation

--- shelf/dataloaders/cifar.py
+++ shelf/dataloaders/cifar.py
@@ -2,17 +2,20 @@ import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 
+from .augmentation import CIFAR10Policy
+
 def get_CIFAR10_dataset(root='./data', batch_size=128):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     train_loader = torch.utils.data.DataLoader(
         datasets.CIFAR10(root=root, train=True, transform=transforms.Compose([
             transforms.RandomHorizontalFlip(),
+            CIFAR10Policy(fillcolor=(125, 122, 113)),
             transforms.RandomCrop(32, 4),
             transforms.ToTensor(),
             normalize,
         ]), download=True),
-        batch_size=batch_size, shuffle=True,
+        batch_size=batch_size, shuffle=False,
         num_workers=1, pin_memory=True)
 
     val_loader = torch.utils.data.DataLoader(
@@ -31,11 +34,12 @@ def get_CIFAR100_dataset(root='./data', batch_size=128):
     train_loader = torch.utils.data.DataLoader(
         datasets.CIFAR100(root=root, train=True, transform=transforms.Compose([
             transforms.RandomHorizontalFlip(),
+            CIFAR10Policy(fillcolor=(125, 122, 113)),
             transforms.RandomCrop(32, 4),
             transforms.ToTensor(),
             normalize,
         ]), download=True),
-        batch_size=batch_size, shuffle=True,
+        batch_size=batch_size, shuffle=False,
         num_workers=1, pin_memory=True)
 
     val_loader = torch.utils.data.DataLoader(
